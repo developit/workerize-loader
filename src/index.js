7cfd8bde55cb102b7b74cb5fe507a753fa9ebd91
Jasper <well.caffeinated@gmail.com>
2019-05-23 13:22:49 -0600

    Update index.js
    https://github.com/webpack-contrib/worker-loader/pull/175

--- src/index.js
+++ src/index.js
@@ -63,6 +63,24 @@ loader.pitch = function(request) {
 	if (this.target!=='webworker' && this.target!=='web') {
 		(new NodeTargetPlugin()).apply(worker.compiler);
 	}
+	
+	// webpack >= v4 supports webassembly
+	let wasmPluginPath = null;
+	try {
+		wasmPluginPath = require.resolve(
+		 	'webpack/lib/web/FetchCompileWasmTemplatePlugin'
+		);
+	} catch (_err) {
+		// webpack <= v3, skipping
+	}
+
+	if (wasmPluginPath) {
+		// eslint-disable-next-line global-require, import/no-dynamic-require
+		const FetchCompileWasmTemplatePlugin = require(wasmPluginPath);
+		new FetchCompileWasmTemplatePlugin({
+		 	mangleImports: this._compiler.options.optimization.mangleWasmImports,
+		}).apply(worker.compiler);
+	}
 
 	(new SingleEntryPlugin(this.context, `!!${path.resolve(__dirname, 'rpc-worker-loader.js')}!${request}`, 'main')).apply(worker.compiler);
 

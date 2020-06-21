04c8eb0fede29c331d30bd5288ffaf2c4a1c98e7
Kevin Decker <kpdecker@gmail.com>
2020-06-20 22:45:32 -0500

    Add support for imported exports
    Adds support for
    
    ```
    export * from 'module';
    export { function } from 'module'
    ```
    
    providedExports field appears to be around for all of Webpack 4. If prior webpacks are supported still, then will need a simple adapter layer.

--- src/index.js
+++ src/index.js
@@ -128,7 +128,12 @@ loader.pitch = function(request) {
 
 			let key = entries[0].entryModule.nameForCondition();
 			let contents = compilation.assets[worker.file].source();
-			let exports = Object.keys(CACHE[key] || {});
+			let exports = Array.from(
+				new Set([
+					...Object.keys(CACHE[key] || {}),
+					...entries[0].entryModule.buildMeta.providedExports
+				])
+			);
 
 			// console.log('Workerized exports: ', exports.join(', '));
 
